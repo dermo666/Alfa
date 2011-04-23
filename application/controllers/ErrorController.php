d9534904cd4b06ddc64eec4643bf9cf83de041aa
Tomas Dermisek <dermisek@gmail.com>
2011-04-23 15:56:54 +1000

    created zf2 quickstart

---
+++ application/controllers/ErrorController.php
@@ -0,0 +1,51 @@
+<?php
+
+class ErrorController extends Zend\Controller\Action
+{
+
+    public function errorAction()
+    {
+        $errors = $this->_getParam('error_handler');
+        
+        switch ($errors->type) {
+            case \Zend\Controller\Plugin\ErrorHandler::EXCEPTION_NO_ROUTE:
+            case \Zend\Controller\Plugin\ErrorHandler::EXCEPTION_NO_CONTROLLER:
+            case \Zend\Controller\Plugin\ErrorHandler::EXCEPTION_NO_ACTION:
+        
+                // 404 error -- controller or action not found
+                $this->getResponse()->setHttpResponseCode(404);
+                $this->view->vars()->message = 'Page not found';
+                break;
+            default:
+                // application error
+                $this->getResponse()->setHttpResponseCode(500);
+                $this->view->vars()->message = 'Application error';
+                break;
+        }
+        
+        // Log exception, if logger available
+        if (($log = $this->getLog())) {
+            $log->crit($this->view->vars()->message, $errors->exception);
+        }
+        
+        // conditionally display exceptions
+        if ($this->getInvokeArg('displayExceptions') == true) {
+            $this->view->vars()->exception = $errors->exception;
+        }
+        
+        $this->view->vars()->request = $errors->request;
+    }
+
+    public function getLog()
+    {
+        $bootstrap = $this->getInvokeArg('bootstrap');
+        if (!$bootstrap->hasPluginResource('Log')) {
+            return false;
+        }
+        $log = $bootstrap->getResource('Log');
+        return $log;
+    }
+
+
+}
+
